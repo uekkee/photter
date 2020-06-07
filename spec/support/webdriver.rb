562c10c64355d1e2bd7529917822478ac05d8604
uekkee <uekkee@gmail.com>
2020-06-07 18:15:38 +0900

    setup headless chrome for github action

---
+++ spec/support/webdriver.rb
@@ -0,0 +1,10 @@
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    if ENV.fetch("HEADLESS_E2E", nil).to_i.positive?
+      driven_by :selenium_chrome_headless, screen_size: [1600, 1600] do
+        driver_options.add_argument("--disable-dev-sim-usage")
+        driver_options.add_argument("--no-sandbox")
+      end
+    end
+  end
+end
