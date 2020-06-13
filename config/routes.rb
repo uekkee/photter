36a28c3e5a61de8c9ed20a7297d7c7705e046d8a
uekkee <uekkee@gmail.com>
2020-06-13 12:27:06 +0900

    add api/images#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ require "sidekiq/web"
 Rails.application.routes.draw do
   resources :images, only: %i(index)
 
+  namespace :api, format: "json" do
+    resources :images, only: :index
+  end
+
   mount Sidekiq::Web => "/sidekiq"
 end
