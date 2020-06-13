c323ff82a86299db3e61381b420b65cc71fcb896
uekkee <uekkee@gmail.com>
2020-06-13 15:03:19 +0900

    write bad-code purposely

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "./css/application.scss"
 
 Rails.start()
 
+console.log('hoge')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
