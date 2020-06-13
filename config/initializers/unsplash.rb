63eb55f1af256ee41df238fd00b8042f2481c40e
uekkee <uekkee@gmail.com>
2020-06-13 12:19:49 +0900

    unable unsplash and VCR on development

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,6 @@
+Unsplash.configure do |config|
+  config.application_access_key = Rails.application.credentials.unsplash[:access_key]
+  config.application_secret = Rails.application.credentials.unsplash[:secret_key]
+  config.utm_source = "photter-demo"
+  # config.application_redirect_uri = "https://your-application.com/oauth/callback"
+end
