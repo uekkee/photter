424e06cac7eb7d4df516a7e12a0eb2f0a62f3b5b
uekkee <uekkee@gmail.com>
2020-07-10 07:36:11 +0900

    add validation for Tag#name

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,6 @@
 class Tag < ApplicationRecord
   has_many :image_tags, inverse_of: :tag, dependent: :delete_all
   has_many :images, through: :image_tags, inverse_of: :tags
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -7,4 +7,9 @@ describe Tag, type: :model do
     it { is_expected.to have_many(:image_tags).inverse_of(:tag).dependent(:delete_all) }
     it { is_expected.to have_many(:images).through(:image_tags).inverse_of(:tags) }
   end
+
+  describe 'validation' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  end
 end
