5c1a6917c5a44121ce5149bc465e0b978fe7afa5
uekkee <uekkee@gmail.com>
2020-07-10 08:47:08 +0900

    add Image#apply_tags_by_name

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,4 +5,8 @@ class Image < ApplicationRecord
   has_many :tags, through: :image_tags, inverse_of: :images
 
   validates :url, presence: true, url: true, uniqueness: { case_sensitive: true }
+
+  def apply_tags_by_name(tag_names)
+    self.tag_ids = tag_names.map { |tag_name| Tag.find_or_create_by(name: tag_name).id }
+  end
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -13,4 +13,22 @@ describe Image, type: :model do
     it { is_expected.to validate_uniqueness_of(:url) }
     it { is_expected.to validate_url_of(:url) }
   end
+
+  describe "#apply_tags_by_name" do
+    subject { image.apply_tags_by_name tag_names }
+
+    let(:image) { create :image }
+    let(:tag_names) { %w(dog pretty puppy) }
+    let!(:exist_tag) { create :tag, name: 'dog' }
+
+    it do
+      subject
+      expect(image.tags).to include exist_tag
+      expect(image.tags.map(&:name)).to match_array tag_names
+    end
+
+    it do
+      expect { subject }.to change { Tag.count }.from(1).to(3)
+    end
+  end
 end
