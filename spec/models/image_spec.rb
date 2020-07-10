0daf1b8b9241509378f5e80b7b851e8bb231a53d
uekkee <uekkee@gmail.com>
2020-07-10 09:12:02 +0900

    add Image.register_with_tag_names

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -21,14 +21,54 @@ describe Image, type: :model do
     let(:tag_names) { %w(dog pretty puppy) }
     let!(:exist_tag) { create :tag, name: 'dog' }
 
-    it do
-      subject
-      expect(image.tags).to include exist_tag
-      expect(image.tags.map(&:name)).to match_array tag_names
+    context "image without tag" do
+      it do
+        subject
+        expect(image.tags).to include exist_tag
+        expect(image.tags.map(&:name)).to match_array tag_names
+      end
+      it do
+        expect { subject }.to change { Tag.count }.from(1).to(3)
+      end
     end
 
-    it do
-      expect { subject }.to change { Tag.count }.from(1).to(3)
+    context "image with tag" do
+      let(:image) { create :image, tags: [bird_tag] }
+      let(:bird_tag) { create :tag, name: 'bird' }
+      it 'tags are overwritten' do
+        subject
+        expect(image.tags).not_to include bird_tag
+        expect(image.tags.map(&:name)).to match_array tag_names
+      end
+    end
+  end
+
+  describe ".register_with_tag_names" do
+    subject { Image.register_with_tag_names image_url: image_url, tag_names: tag_names }
+
+    let(:image_url) { 'https://localhost.localdomain/dog.jpg' }
+    let(:tag_names) { %w(cat cool kitty) }
+
+    context 'no same image_url' do
+      it do
+        expect { subject }.to change { Image.count }.to(1)
+        image = Image.first
+        expect(image).to have_attributes url: image_url
+        expect(image.tags.map(&:name)).to match_array tag_names
+      end
+    end
+
+    context 'has same image_url already' do
+      let(:image) { create :image, url: image_url }
+      it do
+        expect { subject }.to change { image.tags.count }.from(0).to(3)
+        expect(image.tags.map(&:name)).to match_array tag_names
+      end
+    end
+
+    context 'invalid url' do
+      let(:image_url) { 'not a url' }
+      it { expect { subject }.to raise_error ActiveRecord::RecordInvalid }
     end
   end
 end
